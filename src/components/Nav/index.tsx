83352b5782b15d1766a73624c4ff9af8119a995b
mousonin <76007758+mousonin@users.noreply.github.com>
2023-01-31 21:23:38 +0700

    nothing

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,23 @@
+import { FcSearch } from "react-icons/fc";
+import { CiHeart, CiUser} from "react-icons/ci"
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <div className="flex mx-4 items-center">
+        <h4 className="uppercase font-bold text-primary py-4 border-b border-primary">
+          Phimmoi.net
+        </h4>
+        <span className="flex items-center border-solid border rounded-xl border-slate-400 p-1">
+          <input className="outline-none mx-2" placeholder="Search" type="text" />
+          <FcSearch />
+        </span>
+        <span className="flex items-center">
+        <CiUser/>
+        <CiHeart/>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default index;
