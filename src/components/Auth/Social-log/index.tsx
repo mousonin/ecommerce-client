d6e8c16404f72dae38177cb43781db8798e19542
mousonin <songiai126@gmail.com>
2023-02-02 17:13:32 +0700

    refactor with 1 func

--- src/components/Auth/Social-log/index.tsx
+++ src/components/Auth/Social-log/index.tsx
@@ -12,52 +12,23 @@ import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const SocialLog = () => {
-  // const sociaList = [
-  //   {
-  //     funcName: () => GoogleLogin(),
-  //     method: () => new GoogleAuthProvider(),
-  //   },
-  //   {
-  //     funcName: () => FacebookLogin(),
-  //     method: () => new FacebookAuthProvider(),
-  //   },
-  // ];
-
-  // sociaList.map((social) => {
-  //   const {social.funcName} = async () => {
-  //     try {
-  //       const result = await signInWithPopup(auth, social.method());
-  //       console.log(result);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  // });
-
-  const GoogleLogin = async () => {
+  async function socialLogin(by: any) {
     try {
-      const result = await signInWithPopup(auth, new GoogleAuthProvider());
+      const result = await signInWithPopup(auth, by);
       console.log(result);
     } catch (err) {
       console.log(err);
     }
-  };
-  const FacebookLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, new FacebookAuthProvider());
-      console.log(result);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  }
+
   const characters = [
     {
-      onClick: () => GoogleLogin,
+      onClick: () => socialLogin(new GoogleAuthProvider()),
       pText: "Google",
       icon: <FcGoogle className="translate-y-3 duration-1000 opacity-0" />,
     },
     {
-      onClick: () => FacebookLogin,
+      onClick: () => socialLogin(new FacebookAuthProvider()),
       pText: "Facebook",
       icon: (
         <FaFacebookF className="translate-y-3 duration-1000 opacity-0 text-azure-500" />
@@ -74,7 +45,7 @@ const SocialLog = () => {
       {characters.map((character) => (
         <button
           className={`flex items-center border rounded-xl w-full justify-center p-1 m-1 bg-mateColors-0 ${styles["social-log"]}`}
-          onClick={character.onClick()}
+          onClick={character.onClick}
         >
           <p className="m-1 text-white">Continue with {character.pText}</p>
           {character.icon}
